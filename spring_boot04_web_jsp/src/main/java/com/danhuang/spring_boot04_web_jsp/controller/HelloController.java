9c6d73f1e4c610066510789cec738cd64b2edd0e
GodWithegg <675175647@qq.com>
2019-06-16 12:07:37 +0800

    springboot合集+笔记

--- spring_boot04_web_jsp/src/main/java/com/danhuang/spring_boot04_web_jsp/controller/HelloController.java
+++ spring_boot04_web_jsp/src/main/java/com/danhuang/spring_boot04_web_jsp/controller/HelloController.java
@@ -1,4 +1,17 @@
 package com.danhuang.spring_boot04_web_jsp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class HelloController {
+
+    @GetMapping("/abc")
+    public String hello(Model model){
+        model.addAttribute("msg","你好");
+
+        return "success";
+    }
 }
