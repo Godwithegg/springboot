95e8130c30b09d391dc609d4e0d5563435794acd
Godwithegg <675175647@qq.com>
2019-06-18 21:16:58 +0800

    springboot合集

---
+++ spring_boot07_application/src/main/java/com/danhuang/spring_boot07_application/listener/HelloApplicationContextInitializer.java
@@ -0,0 +1,11 @@
+package com.danhuang.spring_boot07_application.listener;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class HelloApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        System.out.println("init method..."+applicationContext);
+    }
+}
